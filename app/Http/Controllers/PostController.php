7f8e0315d51df2d0ea1b8015839f92aa37416be4
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-06-28 20:13:23 +0100

    Building

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use App\Http\Requests\StorePostRequest;
-use App\Http\Requests\UpdatePostRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
@@ -24,14 +24,24 @@ class PostController extends Controller
     public function create()
     {
         //
+
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StorePostRequest $request)
+    public function store(Request $request)
     {
-        //
+
+        // Validate
+        $fields = $request->validate([
+            'title' => ['required', 'max:255'],
+            'body' => ['required']
+        ]);
+
+        // Create a Post
+        Auth::user()->posts()->create($fields);
+        return back()->with('success', 'Your post was created');
     }
 
     /**
@@ -53,7 +63,7 @@ class PostController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdatePostRequest $request, Post $post)
+    public function update(Request $request, Post $post)
     {
         //
     }
--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -1,3 +1,35 @@
 <x-layout>
     <h1 class="title">Hello {{ auth()->user()->username }}</h1>
+    {{-- Create Post Form  --}}
+    <div class="mb-4 card">
+        <h2 class="font-bold mb-4">Create a new post</h2>
+        {{-- Session Messages --}}
+        <div>
+            <p> {{ session('success') }} </p>
+        </div>
+        <form action="{{ route('posts.store') }}" method="POST">
+            @csrf
+            {{-- Title --}}
+            <div class="mb-4">
+                <label for="title">Post Title</label>
+                <input type="text" name="title" value="{{ old('title') }}"
+                    class="input @error('title')
+                    
+                @enderror ring-red-500">
+                @error('title')
+                    <p>{{ $message }}</p>
+                @enderror
+            </div>
+            <div class="mb-4">
+                <label for="body">Post Body</label>
+                <textarea name="body" cols="30" rows="5">
+                    {{ old('body') }}
+                </textarea>
+                @error('body')
+                    <p>{{ $message }}</p>
+                @enderror
+                <button class="btn">Create</button>
+            </div>
+        </form>
+    </div>
 </x-layout>
